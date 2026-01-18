a63ba0892feacd458c676b7e12fec1bb3309ad3c
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-01-18 22:24:38 +0700

    passed test 2

--- Kedua/09-1-transform-stream/app.js
+++ Kedua/09-1-transform-stream/app.js
@@ -2,3 +2,14 @@
  * @TODO
  * Buatlah fungsi `createBase64TransformStream` yang mengonversi data yang masuk menjadi base64.
  */
+const { Transform } = require("stream");
+
+function createBase64TransformStream() {
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      const base64Data = chunk.toString("base64");
+      this.push(base64Data);
+      callback();
+    },
+  });
+}
