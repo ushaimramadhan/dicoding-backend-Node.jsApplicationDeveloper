df64b3bc714db80e0bfc37ea4fb3b45448f88123
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-01-18 22:42:25 +0700

    passed test 6

--- Kedua/11-2-os-uptime-and-memory/app.js
+++ Kedua/11-2-os-uptime-and-memory/app.js
@@ -1,6 +1,8 @@
+const os = require("os");
+
 setTimeout(() => {
-  console.log(null); // @TODO cetak uptime dari process
-  console.log(null); // @TODO cetak uptime dari sistem operasi
-  console.log(null); // @TODO cetak total memori yang tersedia di sistem operasi
-  console.log(null); // @TODO cetak total heap memori
+  console.log(process.uptime()); // @TODO cetak uptime dari process
+  console.log(os.uptime()); // @TODO cetak uptime dari sistem operasi
+  console.log(os.totalmem()); // @TODO cetak total memori yang tersedia di sistem operasi
+  console.log(process.memoryUsage().heapTotal); // @TODO cetak total heap memori
 }, 1000);
